e5817dcbc625fa26a6a406fa3958374eab20d981
¨agus¨ <aguspizarro49@gmail.com>
2022-10-18 18:35:48 -0300

    segunda pre-entrega-2

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,28 +4,22 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-          <a class="navbar-brand" href="#">
+          <Link class="navbar-brand" to = '/'>
             <img class="logoPag" src="/imagenes/logo.png" alt="Logo"/>
-          </a>
+          </Link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" style="justify-content: flex-end;">
           <ul class="navbar-nav">
             <li class="nav-item active">
-            <Link to = '/'>
-              <a class="nav-link" href="./index.html">Inicio</a>
-            </Link>  
+              <Link class="nav-link" to = '/'>Inicio</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./precios.html">Suplementos</a>
+            <Link class="nav-link" to = '/suplementos'>Suplementos</Link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="./contacto.html">Equipamiento</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Tienda</a>
-              <span class="sr-only"></span>
+            <Link class="nav-link" to = '/equipamiento'>Equipamiento</Link>
             </li>
           </ul>
         </div>
